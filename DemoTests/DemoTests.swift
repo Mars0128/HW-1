0e640fd6353c8fbca37473b54d7eb5da418869a9
Mars <mars880128@gmail.com>
2024-07-15 11:59:30 +0800

    Initial Commit

---
+++ DemoTests/DemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  DemoTests.swift
+//  DemoTests
+//
+//  Created by Mars on 2024/7/15.
+//
+
+import Testing
+@testable import Demo
+
+struct DemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
